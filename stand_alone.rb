909829a749ebe27601ff1f7a8c6b4aee98bdbe8a
Stefhen Hovland <stefhen@rightscale.com>
2014-06-03 17:37:47 -0400

    new params

--- stand_alone.rb
+++ stand_alone.rb
@@ -3,10 +3,19 @@
 name 'stefhen-standalone'
 rs_ca_ver '20131202'
 short_description 'Simple standalone CAT'
+long_description 'This is a simple single server deployment'
 
 parameter 'cloud' do
   type 'string'
   label 'Cloud'
   description 'Which cloud do you wish to use?'
-  allowed_values 'us-west-2', 'us-east-1'
+  allowed_values 'EC2 us-west-2', 'EC2 us-east-1'
+end
+
+resource 'server', type: 'server' do
+  name 'Standalone Basic Server'
+  cloud $cloud
+  instance_type 'm1.small'
+  securty_groups find('stefhen-sg')
+  server_template_href '/api/server_templates/336642003'
 end
