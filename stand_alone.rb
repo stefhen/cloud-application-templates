e5b501d99cb162bf8d666ca25a0a9831036bb81a
Stefhen Hovland <stefhen@rightscale.com>
2014-06-03 19:16:52 -0400

    add dc

--- stand_alone.rb
+++ stand_alone.rb
@@ -12,9 +12,17 @@ parameter 'cloud' do
   allowed_values 'EC2 us-west-2', 'EC2 us-east-1'
 end
 
+parameter 'datacenter' do
+  type 'string'
+  label 'Datacenter'
+  description 'Datacenter to use'
+  allowed_values 'us-west-2a', 'us-east-1a'
+end
+
 resource 'server', type: 'server' do
   name 'Standalone Basic Server'
   cloud $cloud
+  datacenter $datacenter
   instance_type 'm1.small'
   security_groups find('stefhen-sg')
   server_template_href '/api/server_templates/336642003'
